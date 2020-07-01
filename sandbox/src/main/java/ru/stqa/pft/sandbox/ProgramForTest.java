b55b93cef6c4774e9faeb74a83282d900cdd7412
SingSingSing666 <67631360+SingSingSing666@users.noreply.github.com>
2020-07-01 10:12:23 +0300

    создание классов и объектов (квадрат и прямоугольник)

--- sandbox/src/main/java/ru/stqa/pft/sandbox/ProgramForTest.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/ProgramForTest.java
@@ -3,7 +3,16 @@ package ru.stqa.pft.sandbox;
 public class ProgramForTest {
 
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
-    }
 
+        System.out.println("Hello, World!");
+
+        Square s = new Square(5);
+        System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
+
+
+        Rectangle r = new Rectangle(5, 10);
+        System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
+
+
+    }
 }
